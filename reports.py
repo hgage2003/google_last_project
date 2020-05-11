58e4fa21e4d7d9defcd87515c8b7c185af8a7dff
Andrey Kovalev <hgage2003@gmail.com>
2020-05-11 21:24:54 +0300

    reports.py now works

--- reports.py
+++ reports.py
@@ -2,12 +2,26 @@
 
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
 
-def generate_report(title, paragraph, filename):
+#body is a list of name/weight tuples
+def generate_report(filename, title, fruits):
   styles = getSampleStyleSheet()
   report = SimpleDocTemplate(filename)
   report_title = Paragraph(title, styles["h1"])
+  empty_line = Spacer(1,20)
+  story = [report_title, empty_line]
+
+  for fruit in fruits:
+    fruit_info = "name: {}<br/>weight: {} lbs".format(fruit[0],fruit[1])
+    story.append(Paragraph(fruit_info, styles["BodyText"]))
+    story.append(empty_line)
   
-    
+  report.build(story)
+  
+def test_pdf():
+  fruits = [('apple',1),('cherry',2),('mercedez-benz',8096)]
+  generate_report("./test.pdf", "This is a test PDF for reports.py", fruits)
 
-
+if __name__ == '__main__':
+    test_pdf()
