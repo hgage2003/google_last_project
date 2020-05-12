4db0459557af5ac9b0f8b7d535dffff09ded6f82
Andrey Kovalev <hgage2003@gmail.com>
2020-05-12 17:39:54 +0300

    change of file list generation

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -6,10 +6,8 @@ import requests
 in_dir = os.path.expanduser('~/supplier-data/images')
 url = "http://localhost/upload/"
 
-for image_file in os.listdir(in_dir):
-  _, ext = os.path.splitext(image_file)
-  if not ext == '.jpeg':
-      continue
-  file_name = os.path.join(in_dir, image_file)
+jpeg_files = [f for f in os.listdir(in_dir) if f.endswith('.jpeg')]
+for f in jpeg_files:
+  file_name = os.path.join(in_dir, f)
   with open(file_name, 'rb') as opened:
       r = requests.post(url, files={'file': opened})
