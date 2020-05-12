825c07f6ace01f5adb9411f0fd8affa381effb84
Andrey Kovalev <hgage2003@gmail.com>
2020-05-12 16:50:28 +0300

    cleanup and comments

--- report_email.py
+++ report_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import reports
 import os
 import datetime
+import reports
 import emails
 
 pdf_name = '/tmp/processed.pdf'
@@ -10,26 +10,33 @@ in_dir = os.path.expanduser('~/supplier-data/descriptions')
 
 #returns formatted string for pdf
 def get_fruit_from_dir(in_dir):
+  # list of name/weight pairs for each fruit
   out_list = []
-  filenames = os.listdir(in_dir)
-  for fname in filenames:
-    _, ext = os.path.splitext(fname)
-    if not ext == '.txt':
-      continue
-    full_name = os.path.join(in_dir, fname)
+  # list of .txt files
+  filenames = [f for f in os.listdir(in_dir) if f.endswith('.txt')]
+  for name in filenames:
+    full_name = os.path.join(in_dir, name)
     with open(full_name) as desc_file:
+      # first line - name
       name = desc_file.readline().rstrip()
+      # second line - weight
       weight = desc_file.readline().rstrip()
+      # skip rest of file
+      desc_file.close()
+      # fill list with current fruit
       out_list.append("name: {}<br/>weight: {}".format(
           name, weight))
+  # return one string of friuts, divided by empty lines
   return "<br/><br/>".join(out_list)
 
 def main():
+  #make pdf
   title = "Processed Update on {}".format(
           datetime.date.today().strftime("%B %d, %Y"))
   fruits = get_fruit_from_dir(in_dir)
   reports.generate_report(pdf_name, title, fruits)
 
+  #send email
   sender = 'automation@example.com'
   receiver = '{}@example.com'.format(os.environ['USER'])
   subject = 'Upload Completed - Online Fruit Store'
